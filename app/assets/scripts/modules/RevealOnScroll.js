d6d3037c74501af048bb6d0591079a2a987689d2
Jacob <jacobodonnell96@proton.me>
2024-08-18 14:10:44 -0500

    Created the Sticky Header functionality

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -16,7 +16,6 @@ class RevealOnScroll {
     window.addEventListener(
       'resize',
       debounce(() => {
-        console.log('Resize just ran');
         this.browserHeight = window.innerHeight;
       }, 300)
     );
